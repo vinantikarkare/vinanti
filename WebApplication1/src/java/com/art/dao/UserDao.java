31c70a22e26255d96d8097ad16cad10a358bbc26
vinantikarkare <vinanti0204@gmail.com>
2020-07-10 11:37:37 +0530

    dao

---
+++ WebApplication1/src/java/com/art/dao/UserDao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.art.dao;
+
+import com.art.dto.User;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import com.art.servlet.DBConnection;
+
+/**
+ *
+ * @author hp
+ */
+public class UserDao {
+    
+public static boolean addUser(User user)
+{
+    return false;
+}
+}
