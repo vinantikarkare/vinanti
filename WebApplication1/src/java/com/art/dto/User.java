7f0916d48423db18d6a2f2abf6105804006ed34c
vinantikarkare <vinanti0204@gmail.com>
2020-07-12 00:37:52 +0530

    dto

---
+++ WebApplication1/src/java/com/art/dto/User.java
@@ -0,0 +1,199 @@
+package com.art.dto;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hp
+ */
+public class User {
+    private String firstname;
+    private String lastname;
+    private String dob;
+    private String username;
+    private String password;
+    private String mobile;
+    private String email;
+    private String gender;
+    private String category;
+    private String skypeid;
+    private int id;
+    private String expertdetail;
+    private String topics;
+    private String resume;
+    private String expertprofile;
+    private String date;
+    private String time;
+    private String expertdate;
+    private String experttime;
+    private String video;
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+    
+
+    public String getExpertdate() {
+        return expertdate;
+    }
+
+    public void setExpertdate(String expertdate) {
+        this.expertdate = expertdate;
+    }
+
+    public String getExperttime() {
+        return experttime;
+    }
+
+    public void setExperttime(String experttime) {
+        this.experttime = experttime;
+    }
+    
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getExpertprofile() {
+        return expertprofile;
+    }
+
+    public void setExpertprofile(String expertprofile) {
+        this.expertprofile = expertprofile;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+    
+    public String getTopics() {
+        return topics;
+    }
+
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
+
+    public String getExpertdetail() {
+        return expertdetail;
+    }
+
+    public void setExpertdetail(String expertdetail) {
+        this.expertdetail = expertdetail;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSkypeid() {
+        return skypeid;
+    }
+
+    public void setSkypeid(String skypeid) {
+        this.skypeid = skypeid;
+    }
+
+    
+   
+   
+} 
